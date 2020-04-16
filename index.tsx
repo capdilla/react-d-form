10b4b17556757a13f28e6f6559dc4cbb869b87e8
Carlos Padilla <capdilla.cp@gmail.com>
2020-04-16 11:38:27 +0200

    Remove ReactNativeDForm

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import Core, { GetComponent } from "./src/lib/core";
 import ReactDForm from "./src/react-web";
 
-import ReactNativeDForm from "./src/react-native";
+// import ReactNativeDForm from "./src/react-native";
 
 export default Core;
 
-export { GetComponent, ReactDForm, ReactNativeDForm };
+export { GetComponent, ReactDForm };
