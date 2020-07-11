b37712f7b9ba7eff81e7276de0e515b5509f79d4
Carlos Padilla <capdilla.cp@gmail.com>
2020-07-12 01:14:40 +0200

    Export more functions

--- index.tsx
+++ index.tsx
@@ -1,8 +1,10 @@
 import Core, { GetComponent } from "./src/lib/core";
 import ReactDForm from "./src/react-web";
-
+import { withError } from "./src/helpers/getError";
+import { IFormComponent } from "./src/types";
+type FormComponent = IFormComponent;
 // import ReactNativeDForm from "./src/react-native";
 
 export default Core;
 
-export { GetComponent, ReactDForm };
+export { GetComponent, ReactDForm, withError, FormComponent };
